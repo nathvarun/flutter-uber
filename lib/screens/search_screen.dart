d9ad32b83be44ab376877e2f650a5c3df16af2be
Varun Nath <nathvarun@hotmail.com>
2022-02-16 13:20:37 +0530

    Part 2 Places Autocomplete

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_place/google_place.dart';
 
 class SearchScreen extends StatefulWidget {
   @override
@@ -6,12 +9,92 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  final _startSearchFieldController = TextEditingController();
+  final _endSearchFieldController = TextEditingController();
+
+  late GooglePlace googlePlace;
+  List<AutocompletePrediction> predictions = [];
+  Timer? _debounce;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    String apiKey = 'AIzaSyCibbLQJBOc-jrJpqbOp5SXKH3Y--zIBJQ';
+    googlePlace = GooglePlace(apiKey);
+  }
+
+  void autoCompleteSearch(String value) async {
+    var result = await googlePlace.autocomplete.get(value);
+    if (result != null && result.predictions != null && mounted) {
+      print(result.predictions!.first.description);
+      setState(() {
+        predictions = result.predictions!;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        color: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: const BackButton(color: Colors.black),
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _startSearchFieldController,
+              autofocus: false,
+              style: TextStyle(fontSize: 24),
+              decoration: InputDecoration(
+                  hintText: 'Starting Point',
+                  hintStyle: const TextStyle(
+                      fontWeight: FontWeight.w500, fontSize: 24),
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: InputBorder.none),
+              onChanged: (value) {
+                if (_debounce?.isActive ?? false) _debounce!.cancel();
+                _debounce = Timer(const Duration(milliseconds: 1000), () {
+                  if (value.isNotEmpty) {
+                    //places api
+                    autoCompleteSearch(value);
+                  } else {
+                    //clear out the results
+                  }
+                });
+              },
+            ),
+            SizedBox(height: 10),
+            TextField(
+              controller: _endSearchFieldController,
+              autofocus: false,
+              style: TextStyle(fontSize: 24),
+              decoration: InputDecoration(
+                  hintText: 'End Point',
+                  hintStyle: const TextStyle(
+                      fontWeight: FontWeight.w500, fontSize: 24),
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: InputBorder.none),
+              onChanged: (value) {
+                if (_debounce?.isActive ?? false) _debounce!.cancel();
+                _debounce = Timer(const Duration(milliseconds: 1000), () {
+                  if (value.isNotEmpty) {
+                    //places api
+                    autoCompleteSearch(value);
+                  } else {
+                    //clear out the results
+                  }
+                });
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
