2a687611b8de7050aa893d8d2b09f8260bdc8912
Varun Nath <nathvarun@hotmail.com>
2022-02-18 13:49:03 +0530

    Displaying and Selecting Places

---
+++ lib/screens/map_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MapScreen extends StatefulWidget {
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_uber_youtube/screens/map_screen.dart';
 import 'package:google_place/google_place.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -12,6 +15,12 @@ class _SearchScreenState extends State<SearchScreen> {
   final _startSearchFieldController = TextEditingController();
   final _endSearchFieldController = TextEditingController();
 
+  DetailsResult? startPosition;
+  DetailsResult? endPosition;
+
+  late FocusNode startFocusNode;
+  late FocusNode endFocusNode;
+
   late GooglePlace googlePlace;
   List<AutocompletePrediction> predictions = [];
   Timer? _debounce;
@@ -20,8 +29,19 @@ class _SearchScreenState extends State<SearchScreen> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    String apiKey = 'AIzaSyCibbLQJBOc-jrJpqbOp5SXKH3Y--zIBJQ';
+    String apiKey = 'AIzaSyAeq_m6Xus_Isb8jtVQ7TbZP9Q2hV4shR0';
     googlePlace = GooglePlace(apiKey);
+
+    startFocusNode = FocusNode();
+    endFocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    startFocusNode.dispose();
+    endFocusNode.dispose();
   }
 
   void autoCompleteSearch(String value) async {
@@ -49,6 +69,7 @@ class _SearchScreenState extends State<SearchScreen> {
             TextField(
               controller: _startSearchFieldController,
               autofocus: false,
+              focusNode: startFocusNode,
               style: TextStyle(fontSize: 24),
               decoration: InputDecoration(
                   hintText: 'Starting Point',
@@ -56,7 +77,18 @@ class _SearchScreenState extends State<SearchScreen> {
                       fontWeight: FontWeight.w500, fontSize: 24),
                   filled: true,
                   fillColor: Colors.grey[200],
-                  border: InputBorder.none),
+                  border: InputBorder.none,
+                  suffixIcon: _startSearchFieldController.text.isNotEmpty
+                      ? IconButton(
+                          onPressed: () {
+                            setState(() {
+                              predictions = [];
+                              _startSearchFieldController.clear();
+                            });
+                          },
+                          icon: Icon(Icons.clear_outlined),
+                        )
+                      : null),
               onChanged: (value) {
                 if (_debounce?.isActive ?? false) _debounce!.cancel();
                 _debounce = Timer(const Duration(milliseconds: 1000), () {
@@ -65,6 +97,10 @@ class _SearchScreenState extends State<SearchScreen> {
                     autoCompleteSearch(value);
                   } else {
                     //clear out the results
+                    setState(() {
+                      predictions = [];
+                      startPosition = null;
+                    });
                   }
                 });
               },
@@ -73,6 +109,9 @@ class _SearchScreenState extends State<SearchScreen> {
             TextField(
               controller: _endSearchFieldController,
               autofocus: false,
+              focusNode: endFocusNode,
+              enabled: _startSearchFieldController.text.isNotEmpty &&
+                  startPosition != null,
               style: TextStyle(fontSize: 24),
               decoration: InputDecoration(
                   hintText: 'End Point',
@@ -80,7 +119,18 @@ class _SearchScreenState extends State<SearchScreen> {
                       fontWeight: FontWeight.w500, fontSize: 24),
                   filled: true,
                   fillColor: Colors.grey[200],
-                  border: InputBorder.none),
+                  border: InputBorder.none,
+                  suffixIcon: _endSearchFieldController.text.isNotEmpty
+                      ? IconButton(
+                          onPressed: () {
+                            setState(() {
+                              predictions = [];
+                              _endSearchFieldController.clear();
+                            });
+                          },
+                          icon: Icon(Icons.clear_outlined),
+                        )
+                      : null),
               onChanged: (value) {
                 if (_debounce?.isActive ?? false) _debounce!.cancel();
                 _debounce = Timer(const Duration(milliseconds: 1000), () {
@@ -89,10 +139,63 @@ class _SearchScreenState extends State<SearchScreen> {
                     autoCompleteSearch(value);
                   } else {
                     //clear out the results
+                    setState(() {
+                      predictions = [];
+                      endPosition = null;
+                    });
                   }
                 });
               },
             ),
+            ListView.builder(
+                shrinkWrap: true,
+                itemCount: predictions.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: CircleAvatar(
+                      child: Icon(
+                        Icons.pin_drop,
+                        color: Colors.white,
+                      ),
+                    ),
+                    title: Text(
+                      predictions[index].description.toString(),
+                    ),
+                    onTap: () async {
+                      final placeId = predictions[index].placeId!;
+                      final details = await googlePlace.details.get(placeId);
+                      if (details != null &&
+                          details.result != null &&
+                          mounted) {
+                        if (startFocusNode.hasFocus) {
+                          setState(() {
+                            startPosition = details.result;
+                            _startSearchFieldController.text =
+                                details.result!.name!;
+                            predictions = [];
+                          });
+                        } else {
+                          setState(() {
+                            endPosition = details.result;
+                            _endSearchFieldController.text =
+                                details.result!.name!;
+                            predictions = [];
+                          });
+                        }
+
+                        if (startPosition != null && endPosition != null) {
+                          print('navigate');
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => MapScreen(),
+                            ),
+                          );
+                        }
+                      }
+                    },
+                  );
+                })
           ],
         ),
       ),
